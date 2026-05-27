cb48497fa94473e1b7ff115891ee74ada3cc36ca
Benjamin De Kosnik <bkoz@gnu.org>
2026-05-27 15:42:58 -0700

    add generation script

---
+++ scripts/generate-graphs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+./a60-meta-collection.exe ../data "Olympics 2026" "" "" 0
+
+./a60-meta-collection.country.exe ../data/olympics-2026-week.json "Olympics 2026"
+
+./a60-meta-collection.btiha.exe ../data/olympics-2026-week.json "Olympics 2026"
+
