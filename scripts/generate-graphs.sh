65e72e508def4df46f80d24cca15defa3d90cbee
Benjamin De Kosnik <bkoz@gnu.org>
2026-06-02 10:14:44 -0700

    udpate graph generation

--- scripts/generate-graphs.sh
+++ scripts/generate-graphs.sh
@@ -8,3 +8,12 @@
 
 ./a60-meta-collection.slices.exe ../data/olympics-2026-week.json "Olympics 2026"
 
+
+./a60-carto-geo.exe ../data/olympics-2026-week-00001.geojson
+./a60-carto-geo.exe ../data/olympics-2026-week-00005.geojson
+./a60-carto-geo.exe ../data/olympics-2026-week-00010.geojson
+./a60-carto-geo.exe ../data/olympics-2026-week-00013.geojson
+./a60-carto-geo.exe ../data/olympics-2026-week-00014.geojson
+./a60-carto-geo.exe ../data/olympics-2026-week-00015.geojson
+
+./a60-carto-geo.exe ../data/olympics-2026-cumulative.geojson
